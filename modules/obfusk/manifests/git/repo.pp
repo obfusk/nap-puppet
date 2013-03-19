c9777b652679fe4c30ec7f4eebbee50d46e252d4
Felix C. Stegerman <flx@obfusk.net>
2013-03-19 23:23:36 +0100

    ...

--- modules/obfusk/manifests/git/repo.pp
+++ modules/obfusk/manifests/git/repo.pp
@@ -2,18 +2,18 @@ define obfusk::git::repo (
   $source,
   $path   = $title,
   $pull   = false,
-  $branch = master
+  $branch = master,
 ) {
-  exec { "git clone|$path":
+  exec { "git clone #$path":
     command => "git clone -b $branch $source $path",
     creates => $path,
   }
 
   if ($pull == true) {
-    exec { "git pull|$path":
-      command =>  "git pull origin $branch",
+    exec { "git pull #$path":
+      command => "git pull origin $branch",
       cwd     => $path,
     }
-    Exec["git clone|$path"] -> Exec["git pull|$path"]
+    Exec["git clone #$path"] -> Exec["git pull #$path"]
   }
 }
