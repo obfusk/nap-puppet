90945dc6cf718a920b6df2fcffcbb1d1e8dc4797
Felix C. Stegerman <flx@obfusk.net>
2013-03-19 23:01:05 +0100

    ...

--- test/git-repo.pp
+++ test/git-repo.pp
@@ -1,4 +1,4 @@
-obfusk::git::repo { "/home/vagrant/nap-puppet":
+obfusk::git::repo { "/home/vagrant/nap-puppet__TEST":
   source  => "https://github.com/obfusk/nap-puppet.git",
   pull    => true,
 }
