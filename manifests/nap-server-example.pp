fc91d5eea263b9cd3dfef5a618dfe0202bdebe21
Felix C. Stegerman <flx@obfusk.net>
2013-03-20 19:41:30 +0100

    ...

--- manifests/nap-server-example.pp
+++ manifests/nap-server-example.pp
@@ -1,3 +1,5 @@
+# === Defaults ==
+
 Exec {
   path => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin',
             '/usr/bin', '/sbin', '/bin' ]
@@ -8,9 +10,21 @@ Package {
   provider  => 'aptitude'
 }
 
+
+# === Happy admin ===
+
 $felix_pkgs = [ 'byobu', 'curl', 'git', 'htop', 'tree', 'vim' ]
 
 package { $felix_pkgs: }
+
+exec { 'update-alternatives => editor':
+  command => 'update-alternatives --set editor /usr/bin/vim.basic',
+  unless  => 'test /etc/alternatives/editor -ef /usr/bin/vim.basic',
+}
+Package['vim'] -> Exec['update-alternatives => editor']
+
+
+# === nap server ===
 
 class { 'nap::server':
   # ...
